9ee668d13ae17df07b78087af5324cdcef47089a
Clay Stewart <claytonmstewart@gmail.com>
2013-09-12 13:47:19 -0500

    Fix error in rolls and score method in BowlingGame

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,4 +4,19 @@ class BowlingGame
   def initialize
     @rolls = []
   end
+
+  # Record a roll in the game.
+  #
+  # pins - The Integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
+  def roll(pins)
+  end
+
+  # Returns the Integer score for this game. Expects to be run after all rolls
+  # for the game have been recorded.
+  def score
+    0
+  end
+
 end
